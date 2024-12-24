b84bf5d1e7bdd8ff4aebff3b0b1a171f68146431
mdshahadad <identity.shahadad@gmail.com>
2024-12-24 15:23:06 +0600

    Build logic for check the quantity, update the quantity also update the stock if it is going to the zero

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -10,6 +10,29 @@ const createOrderIntoDB = async (order: TOrder) => {
     throw new Error('Product not found');
   }
 
+  if (!hasProduct.inStock) {
+    throw new Error('Product out of stock');
+  }
+
+  const reducedQuantity = hasProduct.quantity - quantity;
+  console.log(reducedQuantity);
+
+  if (reducedQuantity === 0) {
+    const updateStock = await Product.findByIdAndUpdate(
+      hasProduct._id,
+      { quantity: reducedQuantity, inStock: false },
+      { new: true },
+    );
+    console.log('UpdateStock:', updateStock);
+  } else {
+    const updateProduct = await Product.findByIdAndUpdate(
+      hasProduct._id,
+      { quantity: reducedQuantity },
+      { new: true },
+    );
+    console.log('UpdateProduct:', updateProduct);
+  }
+
   const result = await Order.create(order);
   return result;
 };
